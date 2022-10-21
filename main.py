7837cdbfaba04d4897b97e89047f7accd6ae9406
Yevhenii Diemientiev <yevhenii.diemientiev.pe@gmail.com>
2022-10-21 23:00:35 +0300

    Replace polling way of bot running with webhook

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from aiogram import Bot, Dispatcher, executor, types
+from aiogram import Bot, Dispatcher, types
+from aiogram.utils.executor import start_webhook
 from decouple import config
 
 from const import START_MESSAGE
 from RatesAggregator import RatesAggregator
 
 API_TOKEN = config('TELEGRAM_BOT_API_TOKEN', cast=str)
+WEBHOOK_URL = config('WEBHOOK_URL', cast=str)
+WEBAPP_HOST = config('WEBAPP_HOST', cast=str)
+WEBAPP_PORT = config('WEBAPP_PORT', cast=int)
 
 # Initialize bot and dispatcher
 bot = Bot(token=API_TOKEN)
@@ -26,5 +30,23 @@ async def get_rates(message: types.Message):
     await message.answer(rates_string, disable_web_page_preview = True, parse_mode = 'HTML')
 
 
+async def on_startup(dp):
+    await bot.set_webhook(WEBHOOK_URL)
+    # insert code here to run it after start
+
+
+async def on_shutdown(dp):
+    # Remove webhook (not acceptable in some cases)
+    await bot.delete_webhook()
+
+
 if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates = True)
+    start_webhook(
+        dispatcher=dp,
+        webhook_path='',
+        on_startup=on_startup,
+        on_shutdown=on_shutdown,
+        skip_updates=False,
+        host=WEBAPP_HOST,
+        port=WEBAPP_PORT,
+    )
