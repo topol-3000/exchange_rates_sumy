a5206866c3917ddc69ae548907838ce139db4941
Yevhenii Diemientiev <yevhenii.diemientiev.pe@gmail.com>
2022-10-22 23:02:15 +0300

    Prepare bot for using with AWS Lambda

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from aiogram import Bot, Dispatcher
-from aiogram.utils.executor import start_webhook
+import asyncio
+
+from aiogram import Bot, Dispatcher, types
 from decouple import config
 
 from handlers import get_rates, send_welcome
@@ -10,27 +11,32 @@ WEBAPP_HOST = config('WEBAPP_HOST', cast=str)
 WEBAPP_PORT = config('WEBAPP_PORT', cast=int)
 
 
-async def on_startup(dp: Dispatcher):
-    await bot.set_webhook(WEBHOOK_URL)
-    dp.register_message_handler(send_welcome, commands=['start', 'help'])
-    dp.register_message_handler(get_rates, commands=['get_rates'])
+async def process_event(event, dp: Dispatcher):
+    """Converting an AWS Lambda event to an update and handling that update."""
+    Bot.set_current(dp.bot)
+    update = types.Update.to_object(event)
+    await dp.process_update(update)
 
 
-async def on_shutdown(dp: Dispatcher):
-    # Remove webhook (not acceptable in some cases)
-    await bot.delete_webhook()
+async def register_handlers(dp: Dispatcher):
+    """Registration all handlers before processing update."""
+    dp.register_message_handler(send_welcome, commands = ['start', 'help'])
+    dp.register_message_handler(get_rates, commands = ['get_rates'])
 
 
-if __name__ == '__main__':
-    # Initialize bot and dispatcher
+async def main(event):
+    """Asynchronous wrapper for initializing the bot and dispatcher,
+    and launching subsequent functions."""
+
     bot = Bot(token = API_TOKEN)
     dp = Dispatcher(bot)
-    start_webhook(
-        dispatcher=dp,
-        webhook_path='',
-        on_startup=on_startup,
-        on_shutdown=on_shutdown,
-        skip_updates=False,
-        host=WEBAPP_HOST,
-        port=WEBAPP_PORT,
-    )
+
+    await register_handlers(dp)
+    await process_event(event, dp)
+
+    return 'ok'
+
+
+def lambda_handler(event, context):
+    """AWS Lambda handler."""
+    return asyncio.get_event_loop().run_until_complete(main(event))
